f7c532d4c9101ea739e0f0f66f9b313238e0b575
Sun-2302 <hei.dera.932@gmail.com>
2024-06-27 12:55:14 +0300

    refactor: change year and month to beginDate and endDate

--- src/main/java/school/hei/Calendar.java
+++ src/main/java/school/hei/Calendar.java
@@ -12,29 +12,39 @@ import static java.util.stream.Collectors.*;
 
 @Getter
 public class Calendar {
-    private final int year;
-    private final Month month;
+    private final LocalDate beginDate;
+    private final LocalDate endDate;
     private List<LocalDate> workingDays;
     private List<LocalDate> holidays;
 
-    public Calendar(int year, Month month, List<LocalDate> holidays) {
-        this.year = year;
-        this.month = month;
+    public Calendar(LocalDate beginDate, LocalDate endDate, List<LocalDate> holidays) {
+        this.beginDate = beginDate;
+        this.endDate = endDate;
         this.holidays = holidays;
+        this.workingDays = workingDays();
+    }
+
+    public Calendar(LocalDate beginDate, LocalDate endDate) {
+        this.beginDate = beginDate;
+        this.endDate = endDate;
+        this.holidays = new ArrayList<>();
         this.workingDays = workingDays();
     }
 
     private List<LocalDate> workingDays() {
         List<LocalDate> dayOfTheMonth = new ArrayList<>();
-        LocalDate debutDate = LocalDate.of(year, month, 1);
-        LocalDate endDate = debutDate.withDayOfMonth(debutDate.lengthOfMonth());
 
-        for (LocalDate date = debutDate; !date.isAfter(endDate); date = date.plusDays(1)) {
+        for (LocalDate date = beginDate; !date.isAfter(endDate); date = date.plusDays(1)) {
             dayOfTheMonth.add(date);
         }
 
         return dayOfTheMonth.stream()
                 .filter(day -> !holidays.contains(day))
                 .collect(toList());
+    }
+
+    public void addHoliday(LocalDate holiday){
+        holidays.add(holiday);
+        workingDays.remove(holiday);
     }
 }
